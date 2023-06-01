524a0c304a53034bbf5fdd97ce322acc777dd0a1
Force9871 <104516432+Force9871@users.noreply.github.com>
2023-06-01 09:02:26 +0200

    space.cpp

--- space.cpp
+++ space.cpp
@@ -1,127 +1,91 @@
-//including libraries.
 #include <iostream>
 #include <string>
 
 int main() {
-//declaring all the variables of the planet number as well as the gravity of each planet.
+
   std::cout << "  _________                                          .__       .__     __   \n";
   std::cout << " /   _____/__________    ____  ____   __  _  __ ____ |__| ____ |  |___/  |_ \n";
-  std::cout << " \_____  \\____ \__  \ _/ ___\/ __ \  \ \/ \/ // __ \|  |/ ___\|  |  \   __\\n";
-  std::cout << " /        \  |_> > __ \\  \__\  ___/   \     /\  ___/|  / /_/  >   Y  \  |  \n";
-  std::cout << "/_______  /   __(____  /\___  >___  >   \/\_/  \___  >__\___  /|___|  /__|  \n";
-  std::cout << "        \/|__|       \/     \/    \/               \/  /_____/      \/      \n";
+  std::cout << " \\_____  \\____ \\__  \\ _/ ___\\/ __ \\  \\ \\/ \\/ // __ \\|  |/ ___\\|  |  \\   __\\\n";
+  std::cout << " /        \\  |_> > __ \\\\  \\__\\  ___/   \\     /\\  ___/|  / /_/  >   Y  \\  |  \n";
+  std::cout << "/_______  /   __(____  /\\___  >___  >   \\/\\_/  \\___  >__\\___  /|___|  /__|  \n";
+  std::cout << "        \\/|__|       \\/     \\/    \\/               \\/  /_____/      \\/      \n";
   std::cout << "                        __               __                                 \n";
   std::cout << "_____________  ____    |__| ____   _____/  |_                               \n";
-  std::cout << "\____ \_  __ \/  _ \   |  |/ __ \_/ ___\   __\                              \n";
-  std::cout << "|  |_> >  | \(  <_> )  |  \  ___/\  \___|  |                                \n";
-  std::cout << "|   __/|__|   \____/\__|  |\___  >\___  >__|                                \n";
-  std::cout << "|__|               \______|    \/     \/                                    \n";
-  std::cout << "                                                                            \n";
-  std::cout << "                                                                            \n";
-  int earthgravity;
-  earthgravity = 1;
+  std::cout << "\\____ \\_  __ \\/  _ \\   |  |/ __ \\_/ ___\\   __\\                              \n";
+  std::cout << "|  |_> >  | \\(  <_> )  |  \\  ___/\\  \\___|  |                                \n";
+  std::cout << "|   __/|__|   \\____/\\__|  |\\___  >\\___  >__|                                \n";
+  std::cout << "|__|               \\______|    \\/     \\/                                    \n";
 
-  int earthnumber;
-  earthnumber = 1;
+  double earthWeight;
 
-  int mercurynumber;
-  mercurynumber = 2;
+  int earthnumber = 1;
+  double earthgravity = 1;
 
-  ;double mercurygravity;
-  mercurygravity = 0.38;
+  int mercurynumber = 2;
+  double mercurygravity = 0.38;
 
-  int venusnumber;
-  venusnumber = 3;
+  int venusnumber = 3;
+  double venusgravity = 0.91;
 
-  ;double venusgravity;
-  venusgravity = 0.91;
+  int marsnumber = 4;
+  double marsgravity = 0.38;
 
-  int marsnumber;
-  marsnumber = 4;
+  int jupiternumber = 5;
+  double jupitergravity = 2.34;
 
-  ;double marsgravity;
-  marsgravity = 0.38;
+  int saturnnumber = 6;
+  double saturngravity = 1.06;
 
-  int jupiternumber;
-  jupiternumber = 5;
+  int uranusnumber = 7;
+  double uranusgravity = 0.92;
 
-  ;double jupitergravity;
-  jupitergravity = 2.34;
+  int neptunenumber = 8;
+  double neptunegravity = 1.19;
 
-  int saturnnumber;
-  saturnnumber = 6;
+  int planetchoice = 0;
 
-  ;double saturngravity;
-  saturngravity = 1.06;
+  std::cout << "What is your weight on Earth?\n";
+  std::cin >> earthWeight;
 
-  int uranusnumber;
-  uranusnumber = 7
+  std::cout << "Planet destination:\n";
+  std::cout << "W == weight\n";
+  std::cout << "1 = Earth, W x 1\n";
+  std::cout << "2 = Mercury, W x 0.38\n";
+  std::cout << "3 = Venus, W x 0.91\n";
+  std::cout << "4 = Mars, W x 0.38\n";
+  std::cout << "5 = Jupiter, W x 2.34\n";
+  std::cout << "6 = Saturn, W x 1.06\n";
+  std::cout << "7 = Uranus, W x 0.92\n";
+  std::cout << "8 = Neptune, W x 1.19\n";
 
-  ;double uranusgravity;
-  uranusgravity = 0.92;
-
-  int neptunenumber;
-  neptunenumber = 8
-
-  ;double neptunegravity;
-  neptunegravity = 1.19;
-
-  int planetchoice;
-  planetchoice = 0;
-
-//Asking for user input.
-  std::cout << "what is your earth weight?\n";
-  std::cin >> earthgravity;
-
-  std::cout << "planet destination:\n";
-//Noting all the planet numbers.
-    std::cout << "W == weight\n";
-
-
-    std::cout << "1 = earth, W x 1\n";
-    std::cout << "2 = mercury, W x 0.38\n";
-    std::cout << "3 = venus, W x 0.91\n";
-    std::cout << "4 = mars, W x 0.38\n";
-    std::cout << "5 = jupiter, W x 2.34\n";
-    std::cout << "6 = saturn, W x 1.06\n";
-    std::cout << "7 = uranus, W x 0.92\n";
-    std::cout << "8 = neptune, W x 1.19\n";
-
-//Asking for user input.
   std::cin >> planetchoice;
-//Declaring all the cases for the switch statement.
+
   switch (planetchoice) {
-    case 2 :
-  std::cout << "Your weight on mercury would be \n" << earthgravity *  0.38 << "kilo's\n";
-  break;
-    case 3 :
-  std::cout << "Your weight on venus would be \n" << earthgravity * 0.91 << " kilo's\n";
-  break;
-    case 4 :
-  std::cout << "Your weight on mars would be \n" << earthgravity * 0.38 << " kilo's\n";
-  break;
-    case 5 :
-  std::cout << "Your weight on jupiter would be \n" << earthgravity * 2.34 << " kilo's\n";
-  break;
-    case 6 :
-  std::cout << "Your weight on saturn would be \n" << earthgravity * 1.06 << " kilo's\n";
-  break;
-    case 7 :
-  std::cout << "Your weight on uranus would be \n" << earthgravity * 0.92 << " kilo's\n";
-  break;
-    case 8 :
-          std::cout << "Your weight on neptune would be \n" << earthgravity * 1.19 << " kilo's\n";
-  break;
-    default :
-  std::cout << "unknown\n";
-  break;
-
-
+    case 2:
+      std::cout << "Your weight on Mercury would be " << earthWeight * 0.38 << " kilos\n";
+      break;
+    case 3:
+      std::cout << "Your weight on Venus would be " << earthWeight * 0.91 << " kilos\n";
+      break;
+    case 4:
+      std::cout << "Your weight on Mars would be " << earthWeight * 0.38 << " kilos\n";
+      break;
+    case 5:
+      std::cout << "Your weight on Jupiter would be " << earthWeight * 2.34 << " kilos\n";
+      break;
+    case 6:
+      std::cout << "Your weight on Saturn would be " << earthWeight * 1.06 << " kilos\n";
+      break;
+    case 7:
+      std::cout << "Your weight on Uranus would be " << earthWeight * 0.92 << " kilos\n";
+      break;
+    case 8:
+      std::cout << "Your weight on Neptune would be " << earthWeight * 1.19 << " kilos\n";
+      break;
+    default:
+      std::cout << "Unknown\n";
+      break;
   }
-//Returning user input to zero.
-return 0;
 
-
+  return 0;
 }
-
-
